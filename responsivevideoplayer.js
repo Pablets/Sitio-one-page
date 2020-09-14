d018267cc947865e784f816fb491a2d1dee1e2d1
Pablets <godoypablom@gmail.com>
2020-09-14 00:59:05 -0300

    Cambios en video, sombras y brillo de imagen

--- responsivevideoplayer.js
+++ responsivevideoplayer.js
@@ -22,6 +22,7 @@
  var embed = "https://www.youtube.com/embed/ID?autoplay=1";
  iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
  iframe.setAttribute("frameborder", "0");
+ iframe.setAttribute("showinfo", "0");
  iframe.setAttribute("allowfullscreen", "1");
  this.parentNode.replaceChild(iframe, this);
  }
