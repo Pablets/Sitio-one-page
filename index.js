ae016ed85733b0cd5c867cc5585d730e04158d1e
Pablets <godoypablom@gmail.com>
2020-09-26 18:36:48 -0300

    Nav hidden on timeout
    added set timeout function to hide the menu on a certain amount of time

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/src/sw.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope:',  registration.scope);
-    }).catch(function(error) {
-      console.log('ServiceWorker registration failed:', errror);
-    });
-  }
+/*jshint esversion: 6*/
+
+// if (navigator.serviceWorker) {
+//     navigator.serviceWorker.register('/src/sw.js').then(function(registration) {
+//       console.log('ServiceWorker registration successful with scope:',  registration.scope);
+//     }).catch(function(error) {
+//       console.log('ServiceWorker registration failed:', errror);
+//     });
+//   }
 
 
-  var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
+  let element = document.getElementById('btn');
+  let elementStyle = window.getComputedStyle(element);
+  let elementLeft = elementStyle.getPropertyValue('left');
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("menu").style.top = "0";
-  } else {
-    // setTimeout(function(){}, 10000);
-      document.getElementById("menu").style.top = "-50px";
-
+  } else if (elementLeft != 0) {
+      setTimeout(function () { document.getElementById("menu").style.top = "-50px"; }, 5000);
+  }else{
+    document.getElementById("menu").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 };
